2ee030503666ed759d35330ecf6771a653f2c8d0
russomaclaud <erussomaclaud@gmail.com>
2022-08-27 07:51:11 -0500

    Modificación HttpResponse.badRequest para recibír un error

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -174,4 +174,19 @@ describe('Login Router', () => {
 
         expect(httpResponse.statusCode).toBe(500);
     });
+
+    // test('Should return 400 if an invalid email es provided', async () => {
+    //     const { sut } = makeSut();
+
+    //     const httpRequest = {
+    //         body: {
+    //             email: 'invalid_email@mail.com',
+    //             password: 'any_password',
+    //         },
+    //     };
+    //     const httpResponse = await sut.route(httpRequest);
+
+    //     expect(httpResponse.statusCode).toBe(400);
+    //     expect(httpResponse.body).toEqual(new InvalidParamError('email'));
+    // });
 });
